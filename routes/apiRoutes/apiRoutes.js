e677373437d3327d1ac7117fcbd087e4151f7e34
AD <archied5150@yahoo.com>
2022-04-13 22:26:43 -0500

    post works through the browser now

--- routes/apiRoutes/apiRoutes.js
+++ routes/apiRoutes/apiRoutes.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const { validateNote, postNote } = require('../../lib/api')
-const notes = require('../../db/db.json')
+const { notes } = require('../../db/db.json')
 
 //get request
 router.get('/notes', (req, res) => {
